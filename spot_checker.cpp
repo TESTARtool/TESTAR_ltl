a97060536568ce11526e80391d14ae7b45043133
css <csengers@majori.com>
2019-05-25 23:03:37 +0200

    sandbox added cli argument --stdin results to model.txt and results.txt

--- spot_checker.cpp
+++ spot_checker.cpp
@@ -11,21 +11,27 @@
 #include <chrono>
 #include <ctime>
 #include <sstream>
+#include <fstream>
+
+std::string aut_line;
+std::ofstream aut_file;
+std::ofstream results_file;
 
 
 
 std::chrono::system_clock::time_point clock_start, clock_end;
-std::string automatonfilename;
+std::string automatonfile;
+std::string result;
 std::string formula;
 std::string formulafile;
 spot::parsed_aut_ptr pa;
 spot::bdd_dict_ptr bdd;
 
 
-void setup(){
+void setup_spot(){
     bdd = spot::make_bdd_dict();
 }
-int  load_automaton(std::string hoafile)
+int  load_automaton(const std::string& hoafile)
 {
     pa = parse_aut(hoafile, bdd);
     if (pa->format_errors(std::cerr)) {
@@ -44,20 +50,31 @@ int  load_automaton(std::string hoafile)
 std::string log_elapsedtime(){
     clock_end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = clock_end-clock_start;
-    return "elapsed time: " + std::to_string(elapsed_seconds.count()) + "s\n";
+    return "elapsed time: " + std::to_string(elapsed_seconds.count()) + "s";
 }
-void custom_print(std::ostream& out, spot::twa_graph_ptr& aut);   //declare before
 
+void custom_print(std::ostream& out, spot::twa_graph_ptr& aut, int verbosity );   //declare before
+
+std::string getAutomatonTitle(spot::twa_graph_ptr& aut){
+    auto name = aut->get_named_prop<std::string>("automaton-name");
+    if (name!= nullptr){
+        return *name;
+    }
+    else {
+        return "";
+    }
+}
 std::string check_property( std::string formula, spot::twa_graph_ptr& aut) {
 
     std::ostringstream sout;
-    sout << "=== start of checking property: " << formula << " ===\n";
+    sout << "=== start of checking property: '" << formula << "' on automaton '"<<getAutomatonTitle(aut) <<"' === "<<log_elapsedtime()<<"\n";
     spot::formula f = spot::parse_formula(formula);
     spot::formula nf = spot::formula::Not(f);
     spot::twa_graph_ptr af = spot::translator(bdd).run(nf);
-    custom_print(std::cout, af);
+    //custom_print(std::cout, af,1);
     spot::twa_run_ptr run;
-    if (run = aut->intersecting_run(af)) {
+    run = aut->intersecting_run(af);
+    if (run) {
         sout << "FAIL, with counterexample:  \n" << *run; //needs emptiness.hh
     }
     else {
@@ -65,12 +82,12 @@ std::string check_property( std::string formula, spot::twa_graph_ptr& aut) {
     run = aut->intersecting_run(af);
     sout << "PASS, with witness: \n" << *run;
     }
-    sout << "=== end of checking property: " << formula << " ===\n";
+    sout << "=== end of checking property: '" << formula <<  "' on automaton '"<<getAutomatonTitle(aut) <<"' === "<<log_elapsedtime()<<"\n";
     return sout.str();
 }
 
 //*********************************
-void custom_print(std::ostream& out, spot::twa_graph_ptr& aut)
+void custom_print(std::ostream& out, spot::twa_graph_ptr& aut, int verbosity = 0)
 {
     // We need the dictionary to print the BDDs that label the edges
     const spot::bdd_dict_ptr& dict = aut->get_dict();
@@ -110,25 +127,36 @@ void custom_print(std::ostream& out, spot::twa_graph_ptr& aut)
     out << "Weak: " << aut->prop_weak() << '\n';
     out << "Inherently Weak: " << aut->prop_inherently_weak() << '\n';
     out << "Stutter Invariant: " << aut->prop_stutter_invariant() << '\n';
+    if (verbosity!=0) {
+        // States are numbered from 0 to n-1
+        unsigned n = aut->num_states();
+        for (unsigned s = 0; s < n; ++s) {
+            out << "State " << s << ":\n";
 
-    // States are numbered from 0 to n-1
-    unsigned n = aut->num_states();
-    for (unsigned s = 0; s < n; ++s)
-    {
-        out << "State " << s << ":\n";
-
-        // The out(s) method returns a fake container that can be
-        // iterated over as if the contents was the edges going
-        // out of s.  Each of these edges is a quadruplet
-        // (src,dst,cond,acc).  Note that because this returns
-        // a reference, the edge can also be modified.
-        for (auto& t: aut->out(s))
-        {
-            out << "  edge(" << t.src << " -> " << t.dst << ")\n    label = ";
-            spot::bdd_print_formula(out, dict, t.cond);
-            out << "\n    acc sets = " << t.acc << '\n';
+            // The out(s) method returns a fake container that can be
+            // iterated over as if the contents was the edges going
+            // out of s.  Each of these edges is a quadruplet
+            // (src,dst,cond,acc).  Note that because this returns
+            // a reference, the edge can also be modified.
+            for (auto &t: aut->out(s)) {
+                out << "  edge(" << t.src << " -> " << t.dst << ")\n    label = ";
+                spot::bdd_print_formula(out, dict, t.cond);
+                out << "\n    acc sets = " << t.acc << '\n';
+            }
         }
     }
+}
+void print_usage(std::ostream& out){
+    out << "Usage of the program :  spot_checker --stdin --a <file> --f <formula> --ff <file> \n";
+    out << "commandline options:\n";
+    out << "--stdin   all input is  via standard input stream: first an automaton followed by formulas. \n";
+    out << "          all other arguments are ignored.";
+    out << "--a       mandatory unless --stdin is the argument. filename containing the automaton (HOA format). \n";
+    out << "--f       optional.  the LTL formula/property to check.  \n";
+    out << "--ff      not operational.  filename containing multiple formulas/properties.) \n\n";
+    out << "without a cli formula, the user can supply via stdin a formula/property.) \n";
+    out << "the results are returned via stdout and the system will ask for a new formula.) \n";
+    out << "a blank line will stop the program.) \n";
 }
 //*************************************
 
@@ -136,64 +164,88 @@ int main(int argc, char *argv[])
 
 {
 
-    if (argc!=3 && argc!=5){
-        std::cerr << "Usage of the program :  spot_checker --a <file> --f <formula> --ff <file> \n";
-        std::cerr << "commandline options:\n";
-        std::cerr << "--a  mandatory. filename containing the automaton (HOA format). \n";
-        std::cerr << "--f  optional.  the LTL formula/property to check.  \n";
-        std::cerr << "--ff optional.  filename containing multiple formulas/properties.) \n\n";
-        //std::cerr << "--o  optional.  filename for the results.) \n\n";
-
-        std::cerr << "without a supplied formula, the user can supply via stdin a formula/property.) \n";
-        std::cerr << "the results are returned via stdout and the system will ask for a new formula.) \n";
-        std::cerr << "a blank line will stop the program.) \n";
-        return 1;
-
-    }else{
-        if ( std::string(argv[1]) != "--a"){
-            std::cerr << "first option is not '--a'.\n";
-            std::cerr << "--a  mandatory. filename containing the automaton (HOA format). \n";
-            return 1;
-        }
-        else{
-            automatonfilename=argv[2];
-        }
-        if (argc==5){
-            if (std::string(argv[3]) != "--f" && std::string(argv[3]) != "--ff"){
-                std::cerr << "second option  is not '--f' or '--ff'.\n";
-                std::cerr << "--f  or --ff contain the formula/properties to check.\n";
+    switch(argc) {
+        case 2 :
+            if(std::string(argv[1])== "--stdin")
+                automatonfile=""; //empty implies: stdin must be read
+            else{
+                std::cerr << "single option is not '--stdin'.\n";
+                print_usage(std::cerr);
                 return 1;
             }
+            break;
+        case 3 :
+            if(std::string(argv[1])== "--a")
+                    automatonfile=argv[2];
             else{
-                if (std::string(argv[3] )== "--f") formula=argv[4];
-                else formulafile=argv[4];
+                    std::cerr << "first option out of two is not '--a'.\n";
+                    print_usage(std::cerr);
+                    return 1;
             }
-        }
+            break;
+        case 5 :
+            if(std::string(argv[3]) == "--f")
+                    formula=argv[4];
+            else if (std::string(argv[3])== "--ff")
+                    formulafile=argv[4];
+                 else {
+                std::cerr << "second option  is not '--f or --ff'.\n";
+                print_usage(std::cerr);
+                return 1;
+            }
+            break;
+        default :
+            print_usage(std::cerr);
+            return 1;
     }
+
+
     // do stuff;
     clock_start = std::chrono::system_clock::now();
     std::cout << "Start of LTL model-check."<< '\n'<< '\n';
-    setup();
-    int res  = load_automaton(automatonfilename);
+    setup_spot();
+    if (automatonfile==""){
+        std::cout<<"Supply the model, line by line and end with 'EOF_HOA' and supply formulas: \n";
+        std::remove("model.txt");
+        aut_file.open("model.txt");
+        while (   getline(std::cin , aut_line)) {
+
+            aut_file << aut_line << std::endl;
+            if (aut_line=="EOF_HOA") {
+                aut_file.close();
+                break;
+            }
+        }
+    automatonfile=  "model.txt";
+    }
+    int res  = load_automaton(automatonfile);
     spot::twa_graph_ptr& aut = pa->aut;
-    custom_print(std::cout, aut);
+    std::cout << "Automaton properties:\n";
+    custom_print(std::cout, aut,0);
+    std:: string auttitle =getAutomatonTitle(pa->aut);
     auto name = aut->get_named_prop<std::string>("automaton-name");
     std::string automatontitle;
        if (name!= nullptr){
-
            automatontitle = *name;
        }
        else {
            automatontitle="";}
-    std::cout << "Enter a formula to verify on automaton "<<automatontitle<<'\n';
-    if (formula=="")
-        while (    getline(std::cin , formula)){
-            if (formula =="") break;
-            std::cout<<check_property(formula,pa->aut);
-        }
+    std::cout << "Automaton '" <<automatontitle<<"' loaded === "<<log_elapsedtime()<<'\n';
+    std::remove("results.txt");
+    results_file.open("results.txt");
+
+   if (formula=="") {
+       std::cout << "Enter a formula to verify on automaton " << automatontitle << '\n';
+       while (getline(std::cin, formula)) {
+           if (formula == "") break;
+           result= check_property(formula, pa->aut);
+           std::cout << result;
+           results_file << result  ;
+       }
+       results_file.close();
+   }
     else
         std::cout<<check_property(formula,pa->aut);
-    std::cout <<"reached end of program. status: "<<res<< "\n";
-    std::cout <<log_elapsedtime();
+    std::cout <<"End of LTL model-check. status: "<<res<<" === "<< log_elapsedtime()<< "\n";
     return 0;
 }
